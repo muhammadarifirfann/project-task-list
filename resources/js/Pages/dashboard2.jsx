5ff5ab2e5b61b487177ff21acfa24b94ce7d8952
M arif Irfan <marifirfannnn@gmail.com>
2025-01-03 13:16:30 +0700

    add new usecase

---
+++ resources/js/Pages/dashboard2.jsx
@@ -0,0 +1,3 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants";
+import { Head, Link } from "@inertiajs/react";
